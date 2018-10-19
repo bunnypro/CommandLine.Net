6a1d887e957c7fee82f1cc31781d1ff57e319579
Mubarrid Albar <mubarrid.albar@gmail.com>
2018-10-19 22:45:19 +0700

    implement example

--- examples/Simple/Program.cs
+++ examples/Simple/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Bunnypro.CommandLine.Examples.Simple
+﻿using System;
+using System.Collections.Generic;
+using Bunnypro.CommandLine.Commands;
+using Newtonsoft.Json;
+
+namespace Bunnypro.CommandLine.Examples.Simple
 {
     public static class Program
     {
         public static int Main(string[] args)
         {
-            return 0;
+            var root = new RootCommand();
+            var app = new Application("abc", root);
+            return app.Run(args);
+        }
+    }
+
+    internal class RootCommand : Command
+    {
+        public override IEnumerable<Option> Options => new[]
+        {
+            new Option("name") {ShortName = 'n'}
+        };
+
+        public void Execute(IDictionary<Option, object> options, string first, int second)
+        {
+            Console.WriteLine("With Option");
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+        }
+
+        public void Execute(string first, object second)
+        {
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+        }
+
+        public void Execute(IDictionary<Option, object> options)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(options, Formatting.Indented));
         }
     }
 }
